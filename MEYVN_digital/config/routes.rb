72a07390142d3710ed6b9d69ae935fd6857d4126
Nikita <sh.nickita@list.ru>
2016-05-31 01:56:41 +0300

    Add user authentication

--- MEYVN_digital/config/routes.rb
+++ MEYVN_digital/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'discussions/show'
+  devise_for :users
+  root to: 'welcome#index'
 
   resources :events do
     resources :discussions, only: [:create, :new, :delete, :update, :show]
