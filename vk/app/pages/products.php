73170b01c1b131359e884340a0b69c19f31c0555
Shkaruba Nikita <sh.nickita@list.ru>
2016-10-31 22:09:57 +0300

    Add selection of items amount on page to pages/products.php
    Changelog:
      pages/products.php:
        * Reworked layout a bit
        * Added products amount fetching based on "itemsAmount" get param
        * Added <select id="itemsOnPageSelect"> for showing items amount selection
        * Added .js insertParam(...) function to insert http get param
        * Reworked changeSortBySelect(...) .js function
        * Added changeItemsOnPageSelect(...) .js function
      util/seed_db.php:
        * Changed random products generation amount from 2 to 1000000

--- vk/app/pages/products.php
+++ vk/app/pages/products.php
@@ -2,26 +2,64 @@
 <?php require_once 'util/products_crud.php'; ?>                                                                                                                                                                              
 
 <script>
-	function changeSortBySelect(selectBy) {
-		//alert(selectBy);
-		window.location = "/products?sortBy=" + selectBy;
+    // Add get parameter to current url and then redirect
+	function insertParam(key, value) {
+		key = encodeURI(key); value = encodeURI(value);
+		var kvp = document.location.search.substr(1).split('&');
+
+		var i=kvp.length; var x; while(i--) {
+			x = kvp[i].split('=');
+
+			if (x[0]==key) {
+				x[1] = value;
+				kvp[i] = x.join('=');
+				break;
+			}
+		}
+
+		if(i < 0) {
+			kvp[kvp.length] = [key,value].join('=');
+		}
+
+		//this will reload the page, it's likely better to store this until finished
+		document.location.search = kvp.join('&'); 
 	}
+
+    // Handle sortBySelect change event
+    function changeSortBySelect(selectBy) {
+		insertParam("sortBy", selectBy);
+    }
+
+    // Handle sortBySelect change event
+    function changeItemsOnPageSelect(itemsAmount) {
+		insertParam("itemsAmount", itemsAmount);
+    }
 </script>
 
-<h3>Products (<a href="products/new">Add new</a>)
+<h2>Products (<a href="products/new">Add new</a>)</h2>
+<p>
 Order by:
     <select id="sortBySelect" onchange="changeSortBySelect(value);">
-	  <option value="id" selected="" >id</option>
-	  <option value="price" <?php if ($_GET["sortBy"] === price) echo "selected=\"selected\""; ?> >price</option>
+	  <option value="id" <?php if ($_GET["sortBy"] === "id") echo "selected=\"selected\""; ?> >id</option>
+	  <option value="price" <?php if ($_GET["sortBy"] === "price") echo "selected=\"selected\""; ?> >price</option>
 	</select> 
-</h3>
-<!--//  -->
 
+Items on page: 
+    <select id="itemsOnPageSelect" onchange="changeItemsOnPageSelect(value);">
+	  <option value="10"      <?php if ($_GET["itemsAmount"] == 10)      echo "selected=\"selected\""; ?> >10</option>
+	  <option value="25"      <?php if ($_GET["itemsAmount"] == 25)      echo "selected=\"selected\""; ?> >25</option>
+	  <option value="50"      <?php if ($_GET["itemsAmount"] == 50)      echo "selected=\"selected\""; ?> >50</option>
+	  <option value="100"     <?php if ($_GET["itemsAmount"] == 100)     echo "selected=\"selected\""; ?> >100</option>
+	  <option value="1000"    <?php if ($_GET["itemsAmount"] == 1000)    echo "selected=\"selected\""; ?> >1000</option>
+	  <option value="10000"   <?php if ($_GET["itemsAmount"] == 10000)   echo "selected=\"selected\""; ?> >10000</option>
+	  <option value="100000"  <?php if ($_GET["itemsAmount"] == 100000)  echo "selected=\"selected\""; ?> >100000</option>
+	  <option value="1000000" <?php if ($_GET["itemsAmount"] == 1000000) echo "selected=\"selected\""; ?> >100000</option>
+	</select> 
 <hr/>
+</p>
 
 <?php
-
-// Todo: consider http 'delete' method usage
+// Handle 'delete?' button if POST
 if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["idToDelete"])) {
     $connection = getDbConnection(DB_NAME);
     deleteProduct($connection, $_POST["idToDelete"]);
@@ -29,27 +67,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["idToDelete"])) {
 
     echo "<h1>Sucessfully deleted product with id = " . $_POST["idToDelete"] . "</h1>";
 }
-
 ?>
 
 <ol>
 <?php 
-
     $connection = getDbConnection(DB_NAME);
+    $itemsAmount = empty($_GET["itemsAmount"]) ? 10 : $_GET["itemsAmount"];
 
     switch($_GET["sortBy"]) {
     default:
     case "id":
-        $products = getProductsSortedById($connection, 1, 100);
+        $products = getProductsSortedById($connection, 1, $itemsAmount);
         break;
     case "price":
-        $products = getProductsSortedByPrice($connection, 1, 100);
+        $products = getProductsSortedByPrice($connection, 1, $itemsAmount);
         break;
+    // Special case just for debugging purposes
+    // Todo: remove later
 	case "none":
 		$products = getAllProducts($connection);
 		break;
     }
-
 	
     foreach($products as $product) {
       echo "<li>" . PHP_EOL;
--- vk/app/util/seed_db.php
+++ vk/app/util/seed_db.php
@@ -1,13 +1,10 @@
 <?php
-
 require_once 'products_crud.php';
 
 $connection = getDbConnection(DB_NAME);
-
-// Todo: Add automatic generation of 1_000_000 products
-insertProduct($connection, "Bike", "Test product", 18000, "https://www.google.ru/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi53c7N1vjPAhXlIpoKHUPlCnYQjRwIBw&url=http%3A%2F%2Fwww.wiggle.co.uk%2Fbikes%2F&psig=AFQjCNHfofl9ovwxZh6wSQG-XROq2IZNHA&ust=1477578394241203");
-insertProduct($connection, "Cup of tea", "Test product", 50, "http://pngimg.com/upload/small/cup_PNG1973.png");
-
+for ($i = 0; $i < 1000000; $i++) {
+    insertProduct($connection, ("Product " . $i), "Test product", rand(1, 100000), "http://product_" . $i . ".png");
+}
 closeDbConnection($connection);
 
 ?>
